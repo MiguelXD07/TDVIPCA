ec3499d801993c0d5caa484964c0498b901cb194
Miguel Ferreira <miguelxd006gmail.com>
2025-03-17 12:13:12 +0000

    Player animations

--- firstProject/firstProject/Game1.cs
+++ firstProject/firstProject/Game1.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using firstProject;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-
+using System.Collections.Generic;
+using System.IO;
 
 namespace firstProject;
 
@@ -11,17 +11,18 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
-    private SpriteFont font; //Variavel de fonte de texto
+
     private int nrLinhas = 0;
     private int nrColunas = 0;
-    private Texture2D player, dot, box, wall; //Load images Texture
-    private Player firstProject;
+    private SpriteFont font; // Variavel de fonte de texto
+    //private char[,] level;
+    public char[,] level;
+
+    private Texture2D dot, box, wall; //Load images Texture
+    private Texture2D[] player;
     int tileSize = 64; //potencias de 2 (operações binárias)
-
-    //Privare char[,] level
-    private char[,] level;
+    private Player firstProject;
     public List<Point> boxes;
-
 
     public Game1()
     {
@@ -30,13 +31,21 @@ public class Game1 : Game
         IsMouseVisible = true;
     }
 
+    public enum Direction
+    {
+        Up, Down, Left, Right // 0, 1, 2, 3
+    }
+    public Direction direction = Direction.Down;
+
     protected override void Initialize()
     {
+        // TODO: Add your initialization logic here
         LoadLevel("level1.txt");
 
         _graphics.PreferredBackBufferHeight = tileSize * level.GetLength(1); //definição da altura
         _graphics.PreferredBackBufferWidth = tileSize * level.GetLength(0); //definição da largura
         _graphics.ApplyChanges(); //aplica a atualização da janela
+
 
 
         base.Initialize();
@@ -46,19 +55,29 @@ public class Game1 : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        font = Content.Load<SpriteFont>("File"); //Usa o nome do Ficheiro do sprite
-
-        player = Content.Load<Texture2D>("Character4");
+        font = Content.Load<SpriteFont>("File"); //Use the name of sprite font file ('File')
         dot = Content.Load<Texture2D>("EndPoint_Purple");
         box = Content.Load<Texture2D>("CrateDark_Purple");
         wall = Content.Load<Texture2D>("Wall_Brown");
 
+        player = new Texture2D[4];
+        player[(int)Direction.Down] = Content.Load<Texture2D>("Character4");
+        player[(int)Direction.Up] = Content.Load<Texture2D>("Character7");
+        player[(int)Direction.Left] = Content.Load<Texture2D>("Character1");
+        player[(int)Direction.Right] = Content.Load<Texture2D>("Character2");
+
+
+        // TODO: use this.Content to load your game content here
     }
 
     protected override void Update(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
+
+        if (Keyboard.GetState().IsKeyDown(Keys.R)) Initialize();
+
+        if (Victory()) Exit(); // FIXME: Change current level
 
         firstProject.Update(gameTime);
 
@@ -69,24 +88,24 @@ public class Game1 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
+        // TODO: Add your drawing code here
         _spriteBatch.Begin();
-        //_spriteBatch.DrawString(font, "O texto tem esta fonte", new Vector2(100, 100), Color.Purple);
-        //_spriteBatch.DrawString(font, "Este e o texto 2", new Vector2(100, 300), Color.Black);
-
-
+        //_spriteBatch.DrawString(font, "O texto que quiser", new Vector2(10, 10), Color.White);
+        //_spriteBatch.DrawString(font, "O texto que quiser", new Vector2(100, 300), Color.Black);
         Rectangle position = new Rectangle(0, 0, tileSize, tileSize); //calculo do retangulo a depender do tileSize
-        for (int x = 0; x < level.GetLength(0); x++) //pega a primeira dimensão
+        for (int x = 0; x < level.GetLength(0); x++)  //pega a primeira dimensão
         {
             for (int y = 0; y < level.GetLength(1); y++) //pega a segunda dimensão
             {
                 position.X = x * tileSize; // define o position
                 position.Y = y * tileSize; // define o position
 
+                // Leitura a partir da matriz
                 switch (level[x, y])
                 {
                     //case 'Y':
-                    //    _spriteBatch.Draw(player, position, Color.White);
-                    //    break;
+                    //      _spriteBatch.Draw(player, position, Color.White);
+                    //      break;
                     //case '#':
                     //    _spriteBatch.Draw(box, position, Color.White);
                     //    break;
@@ -97,32 +116,37 @@ public class Game1 : Game
                         _spriteBatch.Draw(wall, position, Color.White);
                         break;
                 }
+
+                // Leitura a partir da Lista boxes
+                foreach (Point b in boxes)
+                {
+                    position.X = b.X * tileSize;
+                    position.Y = b.Y * tileSize;
+                    _spriteBatch.Draw(box, position, Color.White);
+                }
+
+                // Leitura a partir da classe Player
+                position.X = firstProject.Position.X * tileSize; //posição do Player
+                position.Y = (firstProject.Position.Y) * tileSize; //posição do Player
+                _spriteBatch.Draw(player[(int)direction], position, Color.White); //desenha o Player
             }
         }
-        position.X = firstProject.Position.X * tileSize; //posição do Player
-        position.Y = firstProject.Position.Y * tileSize; //posição do Player
-        _spriteBatch.Draw(player, position, Color.White); //desenha o Player
 
-        foreach (Point b in boxes)
-        {
-            position.X = b.X * tileSize;
-            position.Y = b.Y * tileSize;
-            _spriteBatch.Draw(box, position, Color.White);
-        }
 
         _spriteBatch.End();
 
         base.Draw(gameTime);
     }
+
     void LoadLevel(string levelFile)
     {
         boxes = new List<Point>();
-        string[] linhas = File.ReadAllLines($"Content/{levelFile}"); // "Content/" + level
-        int nrLinhas = linhas.Length;
-        int nrColunas = linhas[0].Length;
+        string[] linhas = File.ReadAllLines($"Content/{levelFile}");  // "Content/" + level
+        nrLinhas = linhas.Length;
+        nrColunas = linhas[0].Length;
 
         level = new char[nrColunas, nrLinhas];
-        
+
         for (int x = 0; x < nrColunas; x++)
         {
             for (int y = 0; y < nrLinhas; y++)
@@ -131,11 +155,12 @@ public class Game1 : Game
                 {
                     boxes.Add(new Point(x, y));
                     level[x, y] = ' '; // put a blank instead of the box '#'
+
                 }
                 else if (linhas[y][x] == 'Y')
                 {
                     firstProject = new Player(this, x, y);
-                    level[x, y] = ' '; // put a blank instead of the sokoban 'Y'
+                    level[x, y] = ' '; // put a blank instead of the firstProject 'Y'
                 }
                 else
                 {
@@ -143,6 +168,7 @@ public class Game1 : Game
                 }
 
             }
+
         }
     }
     public bool HasBox(int x, int y) // x e y é a posição do Player
@@ -153,13 +179,23 @@ public class Game1 : Game
         }
         return false;
     }
+
     public bool FreeTile(int x, int y)
     {
-        if (level[x, y] == 'X') return false; // se for uma parede está ocupada
+        if (level[x, y] == 'X') return false;  // se for uma parede está ocupada
         if (HasBox(x, y)) return false; // verifica se é uma caixa
         return true;
-        /* The same as: return level[x,y] != 'X' && !HasBox(x,y); */
+
+        /* The same as:    return level[x,y] != 'X' && !HasBox(x,y);   */
     }
 
+    public bool Victory()
+    {
+        foreach (Point b in boxes) // pecorrer a lista das caixas
+        {
+            if (level[b.X, b.Y] != '.') return false; // verifica se há caixas sem pontos
+        }
+        return true;
+    }
 
 }
--- firstProject/firstProject/obj/Debug/net6.0-windows/firstProject.AssemblyInfo.cs
+++ firstProject/firstProject/obj/Debug/net6.0-windows/firstProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("firstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5345dc3c38850af60cae3ee98f136b4d84ba451d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a18835ba3d41fe132881259e4946b2c18adc8a1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("firstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("firstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
